4709363e8cbc04c609c7918d1ec4aa47e4a6a3a4
debargham14 <debargham14@gmail.com>
2022-01-11 03:03:02 +0530

    First commit in chat application branch

---
+++ src/main/java/com/chat/app/ChatroomApplication.java
@@ -0,0 +1,14 @@
+package com.chat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(ChatroomApplication.class, args);
+	}
+
+}
